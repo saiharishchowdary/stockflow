31caa568454fb130a42e456ecbc27314919bdd2b
saiharishchowdary <saiharish7386@gmail.com>
2026-06-07 10:53:24 +0530

    chore: build for GitHub Pages (HashRouter + base /stockflow/)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import { ToastContainer } from './components/ui/Toast'
@@ -18,11 +18,11 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <App />
         <ToastContainer />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
